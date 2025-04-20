92e3ed4bd49f6de7e5d192719b3950f1f3c9cf53
Scott Lewis <scott@roof12.com>
2025-04-20 15:37:00 -0600

    rename files

--- encoder/run_test.py
+++ encoder/run_test.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
-from train import train
+from test_cnn import test
 
 if __name__ == "__main__":
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description="Train a CNN model on chess data.")
+    parser = argparse.ArgumentParser(description="Test a CNN model on chess data.")
     parser.add_argument(
         "data_path",
         type=str,
         help="Path to the CSV file containing FEN strings and labels",
     )
-    args = parser.parse_args()  # Parse the arguments
+    args = parser.parse_args()
 
-    train(args.data_path)
+    test(args.data_path)
--- encoder/run_train.py
+++ encoder/run_train.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
-from test import test
+from train_cnn import train
 
 if __name__ == "__main__":
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description="Test a CNN model on chess data.")
+    parser = argparse.ArgumentParser(description="Train a CNN model on chess data.")
     parser.add_argument(
         "data_path",
         type=str,
         help="Path to the CSV file containing FEN strings and labels",
     )
-    args = parser.parse_args()
+    args = parser.parse_args()  # Parse the arguments
 
-    test(args.data_path)
+    train(args.data_path)
